ec98d1e311dad4473bdb48b84eb4e0e9d02168ac
Ankit Sagu <ankitsagu007@gmail.com>
2017-05-08 20:01:14 +0000

    recover initialised

---
+++ resize/resize.c
@@ -0,0 +1,120 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "bmp.h"
+
+int main(int argc, char *argv[])
+{
+    // ensure proper usage
+     if (argc != 4)
+    {
+        printf("Usage: resize scale infile outfile\n");
+        return 1;
+    }
+    // remember scale
+    int scale = atoi(argv[1]);
+       
+    // ensure n is is a positive int less than or equal to 100
+    if(scale > 100 || scale < 1)
+    {
+        printf("Scale must be a positive int less than or equal to 100\n");
+        return 1;
+    }
+
+    // remember filenames
+    char *infile = argv[2];
+    char *outfile = argv[3];
+
+    // open input file 
+    FILE *inptr = fopen(infile, "r");
+    if (inptr == NULL)
+    {
+        fprintf(stderr, "Could not open %s.\n", infile);
+        return 2;
+    }
+
+    // open output file
+    FILE *outptr = fopen(outfile, "w");
+    if (outptr == NULL)
+    {
+        fclose(inptr);
+        fprintf(stderr, "Could not create %s.\n", outfile);
+        return 3;
+    }
+
+    // read infile's BITMAPFILEHEADER
+    BITMAPFILEHEADER bf;
+    fread(&bf, sizeof(BITMAPFILEHEADER), 1, inptr);
+
+    // read infile's BITMAPINFOHEADER
+    BITMAPINFOHEADER bi;
+    fread(&bi, sizeof(BITMAPINFOHEADER), 1, inptr);
+
+    // ensure infile is (likely) a 24-bit uncompressed BMP 4.0
+    if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 || 
+        bi.biBitCount != 24 || bi.biCompression != 0)
+    {
+        fclose(outptr);
+        fclose(inptr);
+        fprintf(stderr, "Unsupported file format.\n");
+        return 4;
+    }
+    
+
+    
+    // write outfile's BITMAPFILEHEADER
+    fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outptr);
+
+    // write outfile's BITMAPINFOHEADER
+    fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, outptr);
+
+   
+
+    
+    // allocate storage for holding rgb 
+    RGBTRIPLE *buffer = malloc(sizeof(RGBTRIPLE) * (bi.biWidth));
+
+    // iterate over files rgb
+    for (int i = 0, biHeight = abs(originalHeight); i < biHeight; i++)
+    {
+        int tracker = 0;
+        // iterate over pixels in scanline
+        for (int j = 0; j < originalWidth; j++)
+        {
+            
+            RGBTRIPLE triple;
+            
+            
+            fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
+            
+            
+            for(int count = 0; count < scale; count++)
+            {
+                *(buffer+(tracker)) = triple;
+                tracker++;
+            }
+        }
+            
+        
+        fseek(inptr, originalPadding, SEEK_CUR);
+
+       
+           for(int r = 0; r < scale; r++)
+           {
+                
+           }        
+    }
+
+    
+    free(buffer);
+
+    // close infile
+    fclose(inptr);
+
+    // close outfile
+    fclose(outptr);
+
+    // success
+    return 0;
+}
