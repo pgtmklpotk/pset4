14290664ba81e5a602176cdab10a5c46567ec384
Ankit Sagu <ankitsagu007@gmail.com>
2017-05-08 19:52:48 +0000

    basic variables and file  initialised

--- recover/recover.c
+++ recover/recover.c
@@ -4,6 +4,25 @@
 
 int main(void)
 {
+    FILE* file = fopen("card.raw", "r");
+    
+   
+    if (file == NULL)
+    {
+        fclose(file); 
+        printf("unable to recover file from card.raw \n");
+        return 1;
+    }
+
+    int counter = 0; 
+   
+    unsigned char jpeg[512];
+    
+     char fname[10];
+   
+   
+   
+    FILE* temp = NULL; 
      
     
     
